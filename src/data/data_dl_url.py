961ed340bc0289d6923ca23cb69ae3742e6b0bc6
Thibaut <thi.fortune@gmail.com>
2025-09-07 18:19:16 +0200

    add dictionnaries for data downloading url

---
+++ src/data/data_dl_url.py
@@ -0,0 +1,15 @@
+COMMUNE_GEOCODED_URL = {
+    '2025': "https://datanova.laposte.fr/data-fair/api/v1/datasets/laposte-hexasmal/metadata-attachments/base-officielle-codes-postaux.csv"
+}
+
+DVF_URL = {
+        '2020': 'https://www.data.gouv.fr/api/1/datasets/r/0d16005c-f68e-487c-811b-0deddba0c3f1',
+        '2021': 'https://www.data.gouv.fr/api/1/datasets/r/3942b268-04e7-4202-b96d-93b9ef6254d6',
+        '2022': 'https://www.data.gouv.fr/api/1/datasets/r/b4f43708-c5a8-4f30-80dc-7adfa1265d74',
+        '2023': 'https://www.data.gouv.fr/api/1/datasets/r/bc213c7c-c4d4-4385-bf1f-719573d39e90',
+        '2024': 'https://www.data.gouv.fr/api/1/datasets/r/5ffa8553-0e8f-4622-add9-5c0b593ca1f8'
+        }
+
+LOYERS_URL = {
+    '2024': 'https://www.data.gouv.fr/fr/datasets/r/89956da9-5b9b-41d7-8703-18dbec4d54a2'
+}
