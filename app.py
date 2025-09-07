9d0b196550bc18ffc86bf936ee750f84e3f8ccf1
Thibaut <thi.fortune@gmail.com>
2025-09-07 22:38:59 +0200

    fixed the color scale for dots on the map

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 from src.data_processing.dvf_data_processing import get_dvf_data
 from src.data_processing.loyer_data_processing import get_loyer_data
 from src.data_processing.real_estate_calculation import get_calc_data
-from src.data_processing.map_data_processing import geocode_df
+from src.data_processing.map_data_processing import get_geocoded_data
 
 def footer():
     st.markdown('---')
@@ -19,13 +19,13 @@ def tab_map():
     dvf_df = get_dvf_data()
     loyers_df = get_loyer_data()
     merged_df = get_calc_data(dvf_df, loyers_df)
-    geocoded_df = geocode_df(merged_df).dropna(subset=['latitude', 'longitude'])
-    st.write(geocoded_df.dtypes)  # Affiche le pour
+    geocoded_df = get_geocoded_data(merged_df)
+    st.write(geocoded_df.dtypes)
 
     st.map(geocoded_df,
            latitude='latitude',
            longitude='longitude',
-           color='prof_rate'
+           color='prof_rate_color',
            )
     return
 
@@ -55,7 +55,7 @@ def tab_rawdata():
     st.dataframe(merged_df.head(1000))
 
     st.write("Données géocodées")
-    geocoded_df = geocode_df(merged_df)
+    geocoded_df = get_geocoded_data(merged_df)
     st.dataframe(geocoded_df.head(1000))
 
 def main():
--- src/data_processing/map_data_processing.py
+++ src/data_processing/map_data_processing.py
@@ -3,7 +3,6 @@ import streamlit as st
 
 from src.data.data_dl_url import COMMUNE_GEOCODED_URL
 
-@st.cache_data
 def geocode_df(merged_df: pd.DataFrame) -> pd.DataFrame:
     """
     Géocode le dataframe joint des DVF et des loyers à partir du code commune INSEE
@@ -15,5 +14,27 @@ def geocode_df(merged_df: pd.DataFrame) -> pd.DataFrame:
                                      left_on='INSEE_C',
                                      right_on='code_commune_insee',
                                      how='left')
-    
+    geocoded_comm_df['prof_rate'] = geocoded_comm_df['prof_rate'].astype('float64')
+
     return geocoded_comm_df
+
+def calculate_prof_rate_color(prof_rate: float) -> str:
+    """
+    Retourne une couleur en fonction du taux de profitabilité.
+    """
+    if prof_rate < 4.0:
+        return '#ff0000'
+    elif prof_rate < 8.0:
+        return '#e3ba54'
+    else:
+        return '#32a852'
+    
+@st.cache_data
+def get_geocoded_data(merged_df: pd.DataFrame) -> pd.DataFrame:
+    # Géocoder les données mergées
+    geocoded_df = geocode_df(merged_df)
+    # Supprimer les lignes sans coordonnées géographiques
+    geocoded_df = geocoded_df.dropna(subset=['latitude', 'longitude'])
+    # Ajouter une colonne de couleur en fonction du taux de profitabilité
+    geocoded_df['prof_rate_color'] = geocoded_df['prof_rate'].apply(calculate_prof_rate_color)
+    return geocoded_df
